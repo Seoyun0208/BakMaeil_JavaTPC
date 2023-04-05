79c77aad12c6261a07fcc294ec91e94c81b33e9c
Seoyun0208 <sylph0105@naver.com>
2023-04-05 20:28:40 +0900

    ~ 05. 관계를 이해하라(실습)

--- JavaTPC/src/TPC03.java
+++ JavaTPC/src/TPC03.java
@@ -1,4 +1,5 @@
 import kr.bit.Book;
+import kr.bit.PersonVO;
 
 public class TPC03 {
 
@@ -11,6 +12,29 @@ public class TPC03 {
 		
 		// 책 1권을 저장하기 위한 변수를 선언하시오.
 		Book b;
+		b=new Book();
+		b.title="자바";
+		b.price=15000;
+		b.company="한빛미디어";
+		b.page=700;
+		
+		System.out.print(b.title+"\t");
+		System.out.print(b.price+"\t");
+		System.out.print(b.company+"\t");
+		System.out.println(b.page+"\t");
+	
+		// 사람 1명을 저장하기 위한 변수를 선언하시오.
+		PersonVO p;
+		p=new PersonVO();
+		p.name="박서윤";
+		p.age=31;
+		p.height=157.8f;
+		p.weight=55.4f;
+		
+		System.out.print(p.name+"\t");
+		System.out.print(p.age+"\t");
+		System.out.print(p.height+"\t");
+		System.out.println(p.weight+"\t");
 	}
 
 }
---
+++ JavaTPC/src/kr/bit/PersonVO.java
@@ -0,0 +1,9 @@
+package kr.bit;
+
+// 회원 -> 이름, 나이, 키, 몸무게, ...
+public class PersonVO {
+	public String name;
+	public int age;
+	public float height;
+	public float weight;
+}
