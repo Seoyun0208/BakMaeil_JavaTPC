275438722d9601b60f6063391f770a6ba30fd4d9
Seoyun0208 <sylph0105@naver.com>
2023-04-05 20:10:17 +0900

    ~ 04. 관계를 이해하라(V. D. A)

---
+++ JavaTPC/src/kr/bit/Book.java
@@ -0,0 +1,10 @@
+package kr.bit;
+
+// 책(객체) -> 상태정보(variables / 제목, 가격, 출판사, 페이지 수, ...) + 행위정보(method) 
+
+public class Book {
+	public String title;
+	public int price;
+	public String company;
+	public int page;
+}
